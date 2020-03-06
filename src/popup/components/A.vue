6e263331edd0c00c609e6a8591b8c98127a45e19
wyin125 <wyin125@protonmail.com>
2020-03-05 18:45:36 -0800

    Create mockup UI

---
+++ src/popup/components/A.vue
@@ -0,0 +1,15 @@
+<template>
+  <a :href="href" class="text-primary hover:text-primary-dark"><slot></slot></a>
+</template>
+
+<script>
+export default {
+  name: 'A',
+  props: {
+    href: {
+      type: String,
+      default: 'javascript:;',
+    },
+  },
+};
+</script>
---
+++ src/popup/components/Button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="py-2 px-6 text-white bg-primary hover:bg-primary-dark text-xl"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+};
+</script>
