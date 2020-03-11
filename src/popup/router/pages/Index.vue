975f5f2e7dc42965304ebaa74d9bff20a1b927d6
adam5909099 <adam5909099@gmail.com>
2020-03-10 19:50:01 -0700

    Implement keyword extracting

--- src/popup/router/pages/Index.vue
+++ src/popup/router/pages/Index.vue
@@ -1,40 +1,38 @@
 <template>
   <div class="w-64 py-4 px-2">
-    <h1 class="text-gray-600 text-xl font-medium">{{ title }}</h1>
-    <div class="text-gray-600 text-xl font-normal mt-1">{{ company }}</div>
-    <div class="text-gray-600 mt-2">{{ location }}</div>
+    <template v-if="this.keywords">
+      <h1 class="text-gray-600 text-xl font-medium">{{ title }}</h1>
+      <div class="text-gray-600 text-xl font-normal mt-1">{{ company }}</div>
+      <div class="text-gray-600 mt-2">{{ location }}</div>
 
-    <img :src="logo" :alt="company" class="w-16 h-16 bg-black mt-4" />
+      <img v-if="logo" :src="logo" class="w-16 h-16 mt-4 rounded-sm" />
+      <div v-else :src="logo" class="w-16 h-16 mt-4 bg-black rounded-sm" />
 
-    <ellipsis-card
-      title="Keywords"
-      description="Advertising, production, communications, portfolio, promotion, technology, concept, video, graphic design, layout, direction, production, communications, portfolio, promotion, technology, concept"
-      class="mt-12"
-    ></ellipsis-card>
+      <ellipsis-card title="Keywords" :description="keywords" class="mt-12"></ellipsis-card>
 
-    <ellipsis-card
-      title="Description"
-      description="As a leading worldwide provider of navigation devices and wearable technology, our focus is on developing, designing and supporting superior products. That makes us an engineering and technology company with a keen focus on recruiting talent in those fields. That makes us an engineering and technology company with a keen focus on recruiting talent in those fields."
-      :max-lines="9"
-      class="mt-12"
-    ></ellipsis-card>
+      <ellipsis-card title="Description" :description="description" :max-lines="9" class="mt-12"></ellipsis-card>
 
-    <div class="mt-12 px-4 text-center">
-      <v-button class="w-full">Save to wishlist</v-button>
-    </div>
-    <div class="text-center mt-4"><anchor>couchtocareer.com</anchor></div>
-
-    <div class="text-gray-600 text-xl mt-10">URL</div>
-    <div class="w-full shadow mt-1 p-2">
-      <div class="text-gray-600 text-sm break-all">
-        {{ url }}
+      <div class="mt-12 px-4 text-center">
+        <v-button class="w-full">Save to wishlist</v-button>
       </div>
-    </div>
+      <div class="text-center mt-4"><anchor>couchtocareer.com</anchor></div>
+
+      <div class="text-gray-600 text-xl mt-10">URL</div>
+      <div class="w-full shadow mt-1 p-2">
+        <div class="text-gray-600 text-sm break-all">
+          {{ url }}
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import cheerio from 'cheerio';
+import retext from 'retext';
+import pos from 'retext-pos';
+import keywords from 'retext-keywords';
+import toString from 'nlcst-to-string';
 import VButton from '../../components/VButton';
 import Anchor from '../../components/Anchor';
 import EllipsisCard from '../../components/EllipsisCard';
@@ -47,36 +45,42 @@ export default {
   },
   data() {
     return {
-      title: null,
-      company: null,
-      location: null,
-      logo: null,
-      keywords: null,
-      description: null,
-      url: null,
+      title: '',
+      company: '',
+      location: '',
+      logo: '',
+      keywords: '',
+      description: '',
+      descriptionHtml: '',
+      url: '',
     };
   },
   mounted() {
     chrome.tabs.query({ lastFocusedWindow: true, active: true }, tabs => {
       this.url = tabs[0].url;
 
-      chrome.tabs.sendMessage(tabs[0].id, 'getHtml', html => {
-        console.log('Html', html);
+      chrome.tabs.sendMessage(tabs[0].id, 'getHtml', async html => {
+        const $ = await cheerio.load(html);
+        this.title = $('#vjs-jobtitle').text();
+        this.company = $('#vjs-cn').text();
+        this.location = $('#vjs-loc')
+          .text()
+          .replace(' - ', '');
+        this.logo = $('#vjs-img-cmL').attr('src');
+        this.description = $('#vjs-desc').text();
+        this.descriptionHtml = $('#vjs-desc').html();
+
+        retext()
+          .use(pos)
+          .use(keywords)
+          .process(this.description, (err, file) => {
+            if (err) {
+              return;
+            }
+
+            this.keywords = file.data.keyphrases.map(phrase => phrase.matches[0].nodes.map(value => toString(value)).join('')).join(', ');
+          });
       });
-      // const page = await browser.newPage();
-      // await page.goto(this.url);
-      // const html = await page.evaluate(() => document.body.innerHTML);
-
-      // console.log(html);
-
-      // const $ = await cheerio.load(html);
-      // this.title = $('#vjs-jobtitle').text();
-
-      // this.company = $('#vjs-cn').text();
-      // this.location = $('#vjs-loc')
-      //   .text()
-      //   .replace(' - ', '');
-      // this.logo = $('#vjs-img-cmL').attr('src');
     });
   },
 };
