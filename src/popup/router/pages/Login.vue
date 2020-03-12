d5416a71a29bef4608718663659f40bddfb006d3
wyin125 <wyin125@protonmail.com>
2020-03-11 22:25:27 -0400

    Add login page

---
+++ src/popup/router/pages/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-64 py-4 px-2">
+    <div class="text-gray-600 text-xl text-center">Login</div>
+    <div></div>
+    <input type="text" class="mt-8 w-full text-gray-600 text-sm py-3 px-4 border border-gray-400 focus:outline-none" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+};
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
           default: '#FAAC1E',
           dark: 'rgb(224, 146, 5)',
         },
+        secondary: {
+          default: '#707070',
+          light: '#DDDDDD',
+        },
       },
       boxShadow: {
         default: '0 1px 6px rgba(0, 0, 0, 0.16)',
